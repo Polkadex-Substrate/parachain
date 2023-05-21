8edb0a8e3e16e88ee1231f282b9f0aa64693bd78
gautham <gauthamastro@gmail.com>
2023-05-21 11:52:41 +0530

    Add try runtime to CI

--- run-ci-tests.sh
+++ run-ci-tests.sh
@@ -1,4 +1,5 @@
 RUSTFLAGS="-D warnings" cargo build --features runtime-benchmarks
+RUSTFLAGS="-D warnings" cargo build --features try-runtime
 ./target/debug/parachain-polkadex-node benchmark pallet --pallet "*" --extrinsic "*" --steps 2 --repeat 1
 RUSTFLAGS="-D warnings" cargo build
 cargo test --workspace
